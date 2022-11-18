432748e08097e6959bcea1fe00507bc250f5c94a
DarkMatr126 <95891701+DarkMatr126@users.noreply.github.com>
2022-11-18 06:29:32 -0700

    Update main.js

--- Arnell/The Arnell Website/js/main.js
+++ Arnell/The Arnell Website/js/main.js
@@ -17,7 +17,7 @@ window.addEventListener('load',() => {
     addReview('Reese H.','I always look forward to Mr. Arnell\'s class. He is an awesome teacher and he\'s very funny.\nOne time Mr.Arnell was pretending like he was about to hit me with his stick and then he actually hit me with the stick, on accident though.');
     addReview('Jasmine J.','He is so funny. 100/10');
     addReview('Jane H.','Mr. Arnell is one of the greatest educators not just of the state, but of this nation. This man is single handedly changing the world. One swoosh. At a time.');
-    addReview('Bevin M.','Mr. Arnell is one of the best teachers I have ever had. The day is just frankly boring without having his Class. \n FIVE STARS\n\nWhoever took down Mr/ Arnell\'s graveyard of swoosh\'s, you should be ashamed of yourself and Mr. Arnell is ashamed too.');
+    addReview('Bevin M.','Mr. Arnell is one of the best teachers I have ever had. The day is just frankly boring without having his Class. \n FIVE STARS\n\nWhoever took down Mr. Arnell\'s graveyard of swoosh\'s, you should be ashamed of yourself and Mr. Arnell is ashamed too.');
     addReview('Olivia H.','Mr. Arnell is my favorite teacher and i don\'t even have him. 100/10');
     addReview('Jane R.','Mr. Arnell is the BEST teacher!!! That\'s all I need to say because he is.');
 });
